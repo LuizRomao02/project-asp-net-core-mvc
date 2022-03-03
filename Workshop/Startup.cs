fc5b6ba9356cd5363a8c136e8c46f36f0e1e85a3
LuizRoma02 <luizromao7@outlook.com>
2022-03-03 11:21:53 -0300

    Scaffolding

--- Workshop/Startup.cs
+++ Workshop/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Workshop.Models;
 
 namespace Workshop
 {
@@ -33,6 +35,9 @@ namespace Workshop
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<WorkshopContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WorkshopContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

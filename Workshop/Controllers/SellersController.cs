fc33251d47d9ccf3f298bb11ee75a3edfee708f6
LuizRomao02 <luizromao7@outlook.com>
2022-03-11 11:03:18 -0300

    Delete Sellers

--- Workshop/Controllers/SellersController.cs
+++ Workshop/Controllers/SellersController.cs
@@ -14,6 +14,7 @@ namespace Workshop.Controllers
 
         private readonly SellerService _sellerServices;
         private readonly DepartamentService _departamentService;
+       
 
         public SellersController(SellerService sellerService, DepartamentService departamentService) //construtor para injetar a dependencia
         {
@@ -37,10 +38,33 @@ namespace Workshop.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-
         public IActionResult Create(Seller seller)
         {
             _sellerServices.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerServices.FindById(id.Value);
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerServices.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
